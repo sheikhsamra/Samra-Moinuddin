a535aca19b9e00064a10371537433c207bcf44f4
Samrashaikh <shaikhsamra855@gmail.com>
2025-10-20 20:05:22 +0500

    updated

--- script.js
+++ script.js
@@ -101,39 +101,7 @@ circles.forEach(elem =>{
 
 // mix it up portfolio section
 var mixer = mixitup('.portfolio-gallery')
-
-
-
-// animation on scroll
-const scrollElements = document.querySelectorAll('.fade-in-left, .fade-in-right, .fade-in-up');
-
-const elementInView = (el, offset = 0) => {
-  const elementTop = el.getBoundingClientRect().top;
-  return elementTop <= (window.innerHeight || document.documentElement.clientHeight) - offset;
-};
-
-const displayScrollElement = (element) => {
-  element.classList.add('in-view');
-};
-
-const handleScrollAnimation = () => {
-  scrollElements.forEach(el => {
-    if(elementInView(el, 100)) {
-      displayScrollElement(el);
-    }
-  })
-}
-
-window.addEventListener('scroll', () => {
-  handleScrollAnimation();
-});
-window.addEventListener('load', () => {
-  handleScrollAnimation(); // check on load too
-});
-
-window.addEventListener('scroll', () => {
-  handleScrollAnimation();
-});
+;
 
 
 let menuLi = document.querySelectorAll('header ul li a');
@@ -154,3 +122,11 @@ const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 50);
 })
+
+
+
+AOS.init({
+  duration: 1000,
+  once: true
+});
+
